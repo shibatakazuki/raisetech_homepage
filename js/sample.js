49282acfa94031f48d7d3f62ae018b48fb06ab6d
shibatakazuki <decks.909efx@gmail.com>
2020-08-11 14:02:31 +0900

    jQuery部分不明
    jQuery部分不明

---
+++ js/sample.js
@@ -0,0 +1,8 @@
+$(function () {
+    var main_nav = $('.main-nav');
+    $('nav_roll', main_nav).mouseover(function (e) {
+      $('nav_roll2', this).stop().slideDown('fast');
+    }).mouseout(function (e) {
+      $('nav_roll2', this).stop().slideUp('fast');
+    });
+  });
